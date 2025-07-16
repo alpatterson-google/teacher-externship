a396b13cdfea2628d16d2fe2a3e3f33f665d3427
Leroy Barriga <leroy.barriga@pfisd.net>
2025-07-16 19:45:41 +0000

    New Component

--- src/components/mini_profile/mini_profile_list.jsx
+++ src/components/mini_profile/mini_profile_list.jsx
@@ -1,4 +1,4 @@
-import AlbertPatterson from './albert_patterson/albert_patterson'
+import LeroyBarriga from './leroy_barriga/albert_patterson';
 
 const MiniProfileList = () => {
     return (
@@ -6,7 +6,8 @@ const MiniProfileList = () => {
             <h2 className="text-2xl font-bold text-gray-800 mb-2 border-b-2 border-gray-200 pb-2 w-full text-center">
                 Participants
             </h2>
-            <AlbertPatterson />
+            <LeroyBarriga/>
+            Hello
         </div>
     );
 };
